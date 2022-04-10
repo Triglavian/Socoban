920a4a19b2041c031668ac139df01422050e40e4
Triglavian <cownsdud97@gmail.com>
2022-04-10 23:18:52 +0900

    before arrangement

--- UnitManagement.cs
+++ UnitManagement.cs
@@ -15,4 +15,27 @@ class UnitManagement
 		m_Boxes = new Boxes();
 		m_Player = new Player();
 	}
-}
+	public int IsPushBox(List<Box> p_Box, int p_Stage)  //return pushable box index
+	{
+		for (int i = 0; i < p_Box.Count; ++i)
+		{
+			if (p_Box[i].GetXPosition() == m_Player[p_Stage].GetXPosition() &&
+					p_Box[i].GetYPosition() == m_Player[p_Stage].GetYPosition())
+			{
+				return i;   //return index if ghost push box
+			}
+		}
+		return -1;  //return -1 if ghost don't push box
+	}
+	public void MoveBox(int p_Stage, int p_BoxId, int[,] p_Field, ConsoleKeyInfo p_Key)
+	{
+		Box box = m_Boxes[p_Stage][p_BoxId];
+		box.MoveGhost(p_Key);
+		if (p_Field[box.GetModifYPosition(), box.GetModifXPosition()] != 1) //1 = wall
+		{
+			box.ConfirmMoveing();
+		} else
+		{
+			box.CancelMoveing();
+		}
+	}
